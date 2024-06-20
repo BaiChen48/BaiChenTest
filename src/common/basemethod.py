ce92f1ae2e5fa93d0da7f3e1ff991d9a90652e50
= <=>
2024-06-20 17:49:47 +0800

    2024620

--- src/common/basemethod.py
+++ src/common/basemethod.py
@@ -1,7 +1,19 @@
+import time
+
 from selenium.webdriver.support import  expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
-def presence_of_element_located(ele_located):
-    element = WebDriverWait.until(EC.visibility_of_element_located(ele_located))
+from selenium import webdriver
+def presence_of_element_located(driver,ele_located):
+    element = WebDriverWait(driver,timeout=30,poll_frequency=1).until(EC.visibility_of_element_located(ele_located))
     return element
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.get('https://www.baidu.com')
+
+    baidu_input_ele = presence_of_element_located(driver,(By.CSS_SELECTOR,'[name="wd"]'))
+    baidu_input_ele.send_keys('显示等待')
+    badiu_ele = presence_of_element_located(driver,(By.CSS_SELECTOR,'[value="百度一下"]'))
+    badiu_ele.click()
+    time.sleep(5)
