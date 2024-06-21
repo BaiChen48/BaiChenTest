65cada183dacf5841a102184ff77c70506ee9e84
= <=>
2024-06-21 17:45:36 +0800

    2024621 1745

--- src/tests/Buy.py
+++ src/tests/Buy.py
@@ -20,27 +20,24 @@ class Buy():
 
 
 if __name__ == '__main__':
+    # 淘宝时间
     time = Buy().taobao_time()
+    print("淘宝时间13位时间戳：", time)
+    # 指定时间
     year, month, day, hour, minute, second, = 2024, 6, 20, 15, 30, 0,
-    buy_dt = datetime.datetime(year, month, day, hour, minute, second, microsecond=1)
-
-    print('buy_dt:', buy_dt)
+    buy_dt = datetime.datetime(year, month, day, hour, minute, second)
+    buy_time = int(buy_dt.timestamp())*1000
+    print("特定时间13位时间戳：", buy_time)
+    # 系统时间
     now = datetime.datetime.now()
+    timestamp_milliseconds = int(now.timestamp() * 1000)
+    print("当前时间13位时间戳：", timestamp_milliseconds)
 
-    # 将 datetime 对象转换为时间戳（秒）
-    timestamp_seconds = now.timestamp()
 
-    # 将时间戳转换为毫秒
-    timestamp_milliseconds = int(timestamp_seconds * 1000)
-
-    print("当前时间的 13 位毫秒时间戳：", timestamp_milliseconds)
-
-    print("淘宝 时间 13 位毫秒时间戳：", time)
-
-    dt_object = datetime.datetime.fromtimestamp(time / 1000)
-    formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
-    # 格式化 datetime 对象为字符串，并包含毫秒
-    formatted_time_with_milliseconds = dt_object.strftime('%Y-%m-%d %H:%M:%S.%f')#[:-3]
-
-    print("格式化的日期和时间（包含毫秒）：", formatted_time_with_milliseconds)
-    print("淘宝格式化的日期和时间：", formatted_time)
+    # dt_object = datetime.datetime.fromtimestamp(time / 1000)
+    # formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+    # # 格式化 datetime 对象为字符串，并包含毫秒
+    # formatted_time_with_milliseconds = dt_object.strftime('%Y-%m-%d %H:%M:%S.%f')#[:-3]
+    #
+    # print("格式化的日期和时间（包含毫秒）：", formatted_time_with_milliseconds)
+    # print("淘宝格式化的日期和时间：", formatted_time)
