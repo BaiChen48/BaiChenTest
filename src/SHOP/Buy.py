71d66cf44fe8ab9eed5381609f1b055b8380e0b9
baichen <455661020@qq.com>
2024-06-22 21:23:47 +0800

    2024622 2123

--- src/SHOP/Buy.py
+++ src/SHOP/Buy.py
@@ -21,22 +21,24 @@ class Buy():
             print('开始登录')
             login_element_locat = (By.LINK_TEXT, '亲，请登录')
             presence_of_element_located(self.driver, login_element_locat).click()
-            time.sleep(30)
+
+            time.sleep(10)
             print('点击购物车')
             shopping_car_locat = (By.CSS_SELECTOR, '[href="//cart.taobao.com"]')
             presence_of_element_located(self.driver, shopping_car_locat).click()
             print('勾选全选按钮')
-            # select_all_locat = (By.ID, 'J_SelectAll1')
-            select_all = (By.CSS_SELECTOR, '[for="J_SelectAllCbx1"]')
-            ActionChains(self.driver).context_click(select_all).perform()
-            # select_all = (By.CSS_SELECTOR, 'for="J_CheckShop_s_725677994_1"')
-            presence_of_element_located(self.driver, select_all).click()
+            handles = self.driver.window_handles
+            # print('driver.title:', self.driver.title)
+            self.driver.switch_to.window(handles[1])
+            # print('driver.title:', self.driver.title)
+            select_all_locat = (By.ID, 'J_SelectAll1')
+            presence_of_element_located(self.driver, select_all_locat).click()
             print('点击结算按钮')
             settlement_element_locat = (By.ID, 'J_Go')
             presence_of_element_located(self.driver, settlement_element_locat).click()
 
             # 提交订单按钮
-            submit_order_button = (By.CSS_SELECTOR,'title="提交订单"')
+            submit_order_button = (By.CSS_SELECTOR, 'title="提交订单"')
             presence_of_element_located(self.driver, submit_order_button).click()
         except:
             print('失败')
@@ -45,7 +47,6 @@ class Buy():
         #     print('通过链接进入购物车')
         #     car_url = 'https://cart.taobao.com/cart.htm'
         #     self.driver.get(car_url)
-
 
 
 if __name__ == '__main__':
